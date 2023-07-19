d70a57c69f49dae5bc85ab84741c8ebb0192efd6
Alejandro Rodriguez <37203345+Aleviz@users.noreply.github.com>
2023-07-19 16:40:46 -0600

    Mostrando los datos de los libros en la tabla desde la API a través del servicio LibroServicio

--- Book-Inventory-FrontEnd/src/app/administrar-libro/administrar-libro.component.ts
+++ Book-Inventory-FrontEnd/src/app/administrar-libro/administrar-libro.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LibroService } from '../servicio/libro/libro.service';
 
 @Component({
   selector: 'app-administrar-libro',
   templateUrl: './administrar-libro.component.html',
   styleUrls: ['./administrar-libro.component.css']
 })
-export class AdministrarLibroComponent {
+export class AdministrarLibroComponent implements OnInit {
+
+  libros: any; // Declaración de variable de tipo any(puede almacenar cualquier tipo de dato)
+  
+
+  /* 
+   * Recibe una instancia del servicio LibroService a través de la inyección de dependencia.
+   * Este nos permitira utilizar el servicio para mas adelante poder obtener y mostrar sus datos
+  */
+  constructor(
+    public libroService: LibroService 
+    ){}
+
+
+    /**
+     * este metodo nos permite ejecutar el codigo dentro, despues que el componente
+     * ha sido inicializado, este codigo nos permitira obtener la lista de libros y luego asignar esta lista
+     * a la propiedad libros.
+     */
+  ngOnInit(): void {
+    this.libroService.getAllLibros().subscribe(resp=>{
+      this.libros = resp;
+    });
+  }
 
 }
