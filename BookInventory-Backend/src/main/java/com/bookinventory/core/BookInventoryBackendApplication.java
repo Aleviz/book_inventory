c454c424a84fb8d3dd17f26c12016cf20babef8d
Alejandro Rodriguez <37203345+Aleviz@users.noreply.github.com>
2023-07-19 07:00:09 -0600

    Agregando el proyecto inicial del backend en Java Spring Boot
     Contiene el proyecto inicial del backend desarrollado en Java Spring Boot para la aplicación de administración de libros.

---
+++ BookInventory-Backend/src/main/java/com/bookinventory/core/BookInventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookinventory.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookInventoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookInventoryBackendApplication.class, args);
+	}
+
+}
---
+++ BookInventory-Backend/src/test/java/com/bookinventory/core/BookInventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookinventory.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInventoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
